305d338ae5daad2706e9a9c061517ca5e793bb16
Jeffry Gonzalez <jeff_gonzalez@progressive.com>
2021-08-23 16:21:22 -0400

    done with Monday

---
+++ CSharpNotes/BasicTypes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CSharpNotes
+{
+    public class BasicTypes
+    {
+        [Fact]
+        public void SomeNumericDataTypes()
+        {
+            int age;
+
+            age = 52;
+
+            Assert.Equal(52, age);
+        }
+
+        [Fact]
+        public void SomeStrings()
+        {
+            string name;
+            name = "Roger";
+
+            Assert.Equal("Roger", name);
+        }
+    }
+}
