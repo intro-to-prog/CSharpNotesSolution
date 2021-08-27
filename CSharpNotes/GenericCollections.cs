31d93deeaee921fac04cc49ca0f7aeb8177bd1b4
Jeffry Gonzalez <jeff_gonzalez@progressive.com>
2021-08-27 16:16:43 -0400

    DONE WITH WEEK1

---
+++ CSharpNotes/GenericCollections.cs
@@ -0,0 +1,112 @@
+﻿using CSharpNotes.Domestic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CSharpNotes
+{
+    public class GenericCollections
+    {
+        [Fact]
+        public void OldSkoolCollectionsDontUseThese()
+        {
+            var myFavoriteNumbers = new ArrayList();
+            myFavoriteNumbers.Add(9);
+            myFavoriteNumbers.Add(20);
+            myFavoriteNumbers.Add(108);
+
+
+            Assert.Equal(108, myFavoriteNumbers[2]);
+
+            var firstTwo = ((int)myFavoriteNumbers[0]) + ((int)myFavoriteNumbers[1]);
+
+            Assert.Equal(29, firstTwo);
+
+        }
+
+        [Fact]
+        public void FavoriteNumbersGenerics()
+        {
+            var favoriteNumbers = new List<int>();
+            favoriteNumbers.Add(9);
+            favoriteNumbers.Add(20);
+            favoriteNumbers.Add(108);
+
+            var firstTwo = favoriteNumbers[0] + favoriteNumbers[1];
+
+            Assert.Equal(29, firstTwo);
+        }
+
+        [Fact]
+        public void GenericDictionary()
+        {
+            var friends = new Dictionary<string, Friend>();
+            friends.Add("sean", new Friend("Sean", "Carlin"));
+            friends.Add("david", new Friend("David", "Iozzi"));
+            friends.Add("amy", new Friend("Amy", "Stahl"));
+
+            Assert.Equal("Stahl", friends["amy"].LastName);
+
+            if (friends.ContainsKey("david"))
+            {
+
+            }
+        }
+        [Fact]
+        public void InitializeAndEnumerateAList()
+        {
+            var friends = new List<string> { "Sean", "Billy", "Jamie" };
+
+            foreach (var person in friends)
+            {
+                // do something with that person.
+            }
+
+        }
+        [Fact]
+        public void InitializeAndEnumerateADictionary()
+        {
+            var friends = new Dictionary<string, Friend>()
+            {
+                { "sean", new Friend("Sean", "Carlin") },
+                { "billy", new Friend("Billy", "Farkas") }
+            };
+
+            foreach (var name in friends.Keys)
+            {
+                // sean, billy
+            }
+
+            foreach (var friend in friends.Values)
+            {
+                // Friend("Sean"...), Friend("Billy"...)
+            }
+
+            foreach (KeyValuePair<string, Friend> kvp in friends)
+            {
+                //{ "sean", new Friend("Sean", "Carlin") },
+                //{ "billy", new Friend("Billy", "Farkas") }
+            }
+
+        }
+
+        [Fact]
+        public void ABitAboutRecords()
+        {
+            var kevin = new Friend("Kevin", "Coral");
+            Assert.Equal("Kevin", kevin.FirstName);
+
+            var julie = kevin with { FirstName = "Julie" };
+
+            Assert.Equal("Julie", julie.FirstName);
+            Assert.Equal("Coral", julie.LastName);
+            Assert.Equal("Kevin", kevin.FirstName);
+        }
+
+        public record Friend(string FirstName, string LastName);
+    }
+}
---
+++ CSharpNotes/Properties.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CSharpNotes
+{
+    public class Properties
+    {
+        [Fact]
+        public void UsingProperties()
+        {
+            var bob = new Employee();
+            bob.FirstName = "Robert";
+            Assert.Equal("Robert", bob.FirstName);
+
+            Assert.Equal(180000, bob.Salary);
+
+            bob.GiveRaise(10);
+            Assert.Equal(180010, bob.Salary);
+        }
+
+        [Fact]
+        public void ObjectInitializers()
+        {
+            var sue = new Employee
+            {
+                FirstName = "Susan",
+                LastName = "Jones"
+            };
+
+            var bread = new Product("3939", "Milk", 3.99m, 12);
+           // var beer = new Product()
+            Assert.Equal("Milk", bread.Name);
+
+
+            var dune = new Movie("Dune")
+            {
+                Director = "David Lynch",
+                YearReleased = 1983
+            };
+
+            Assert.Equal("Dune", dune.Title);
+
+            var updatedDune = dune with { YearReleased = 1984 };
+
+
+        }
+    }
+
+    public class Employee
+    {
+        // "Auto Properties"
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Salary { get; private set; } = 180000;
+
+        public void GiveRaise(decimal amount)
+        {
+            Salary += amount;
+        }
+
+
+    }
+    public class Product
+    {
+        public Product(string sku, string name, decimal price, int qty)
+        {
+            Sku = sku;
+            Name = name;
+            Price = price;
+            Qty = qty;
+        }
+        public Product(string sku, string name, decimal price): this(sku, name, price, 0)
+        {
+           
+        }
+        public string Sku { get; private set; }
+        public string Name { get; private set; }
+        public decimal Price { get; private set; }
+        public int Qty { get; private set; }
+    }
+
+    public record Movie
+    {
+        public Movie(string title)
+        {
+            Title = title;
+        }
+        public string Title { get;  }
+        public string Director { get; init; }
+        public int YearReleased { get; init; }
+    }
+}
